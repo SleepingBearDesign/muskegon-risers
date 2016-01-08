5353f778096c3ec7d20dcd210368d6a8074f90b4
Brody Berson <brody.berson@me.com>
2016-01-08 10:34:03 -0700

    Initial Commit

---
+++ public_html/assets/js/interact-register.js
@@ -0,0 +1,6 @@
+function agreement(){
+  document.getElementById("submit_btn").disabled=false;
+}
+function disagreement(){
+  document.getElementById("submit_btn").disabled=true;
+}
---
+++ public_html/blog/wp-content/themes/risers/footer.php
@@ -0,0 +1,28 @@
+			
+		
+		</div> <!-- end #container -->
+
+		<footer id="footerwrap" role="contentinfo">
+				
+			<div id="inner-footer" class="clearfix">
+						
+				<nav class="clearfix">
+					<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+				</nav>
+				<div class="container">
+					<h4>Muskegon Risers Soccer Club - Copyright 2014 - Created by <a href="http://www.sleepingbeardesign.com/" target="_blank">Sleeping Bear Design</a></h4>
+				</div>
+			</div>
+								
+		</footer> <!-- end footer -->
+				
+		<!--[if lt IE 7 ]>
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
+		
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html>
