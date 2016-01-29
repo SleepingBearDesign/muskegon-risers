3d4a1bfadb110243f7d65c6acae6499ded849fb4
Brody Berson <brody.berson@me.com>
2016-01-28 23:32:07 -0700

    Site Updates

--- js/main.js
+++ js/main.js
@@ -25,13 +25,13 @@ $(document).ready(function(){
 	scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
+    // initBannerVideoSize('.video-container .filter');
     initBannerVideoSize('.video-container video');
 
     $(window).on('resize', function() {
         scaleVideoContainer();
         scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container .filter');
+        // scaleBannerVideoSize('.video-container .filter');
         scaleBannerVideoSize('.video-container video');
     });
 });
