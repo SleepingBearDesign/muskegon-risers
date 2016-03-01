a4b66166381de81dcb91d0de258738cdcd595a96
Brody Berson <brody.berson@me.com>
2016-02-29 21:29:13 -0700

    Product Page Updates

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,3 +1,22 @@
+$(window).load(function() {
+    $("#flexiselDemo1").flexisel({
+        enableResponsiveBreakpoints: true,
+        responsiveBreakpoints: {
+            portrait: {
+                changePoint:480,
+                visibleItems: 3
+            },
+            landscape: {
+                changePoint:640,
+                visibleItems: 3
+            },
+            tablet: {
+                changePoint:768,
+                visibleItems: 3
+            }
+        }
+      });
+});
 $( document ).ready(function() {
 
     scaleVideoContainer();
